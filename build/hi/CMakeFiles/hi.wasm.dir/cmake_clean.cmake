f146e7693f19580fb4e09c22aefbfc83d99eb181
zidate <443085262@qq.com>
2019-06-17 16:39:31 +0800

    add contract

---
+++ build/hi/CMakeFiles/hi.wasm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hi.wasm.dir/src/hi.cpp.o"
+  "hi.wasm.pdb"
+  "hi.wasm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hi.wasm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
